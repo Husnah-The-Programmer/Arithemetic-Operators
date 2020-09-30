4702a5f1eee1b4f32fcd9d08cd6a97d13edf4109
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-09-30 17:09:55 +0100

    Add files via upload

---
+++ Arithemetic Operators.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+c = a + b
+print( "Value of a + b = ", c)
+c = a - b
+print( "value of a - b = ", c)
+c = a * b
+print( "value of a * b = ", c)
+c = b / a
+print( "value of b / a = ", c)
+c = b % a
+print("value of b % a = ", c)
